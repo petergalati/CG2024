c46f0464b5977e825bd1f9d8041f940913af9e42
Peter Galati <peter8galati@gmail.com>
2024-10-08 01:13:54 +0100

    texture (kinda)

--- Weekly Workbooks/01 Introduction and Orientation/extras/RedNoise/src/RedNoise.cpp
+++ Weekly Workbooks/01 Introduction and Orientation/extras/RedNoise/src/RedNoise.cpp
@@ -2,6 +2,7 @@
 #include <CanvasPoint.h>
 #include <Colour.h>
 #include <DrawingWindow.h>
+#include <TextureMap.h>
 #include <Utils.h>
 #include <fstream>
 #include <vector>
@@ -147,6 +148,141 @@ void drawFilledTriangle(DrawingWindow &window, CanvasTriangle triangle, Colour c
     }
 }
 
+CanvasPoint getCanvasPixelGivenProportion(CanvasPoint start, CanvasPoint end, float proportion) {
+    float dx = end.x - start.x;
+    float dy = end.y - start.y;
+
+    float new_x = start.x + dx * proportion;
+    float new_y = start.y + dy * proportion;
+
+    return {new_x, new_y};
+}
+
+CanvasPoint getTexturePixelGivenProportion(CanvasPoint start, CanvasPoint end, float proportion) {
+    float dx = end.texturePoint.x - start.texturePoint.x;
+    float dy = end.texturePoint.y - start.texturePoint.y;
+
+    float new_x = start.texturePoint.x + dx * proportion;
+    float new_y = start.texturePoint.y + dy * proportion;
+
+    CanvasPoint new_texture_point;
+    new_texture_point.texturePoint.x = new_x;
+    new_texture_point.texturePoint.y = new_y;
+
+    return new_texture_point;
+}
+
+float getProportionAlongLineGivenPoint(CanvasPoint start, CanvasPoint end, CanvasPoint point) {
+    float start_to_end_x = end.x - start.x;
+    float start_to_end_y = end.y - start.y;
+    float start_end_magnitude = sqrt((start_to_end_x) * (start_to_end_x) + (start_to_end_y) * (start_to_end_y));
+
+    float start_to_point_x = point.x - start.x;
+    float start_to_point_y = point.y - start.y;
+    float start_point_magnitude = sqrt((start_to_point_x) * (start_to_point_x) + (start_to_point_y) * (start_to_point_y));
+
+    return start_point_magnitude / start_end_magnitude;
+}
+
+void drawTexturedTriangle(DrawingWindow &window, CanvasTriangle canvas_triangle, CanvasTriangle texture_triangle, const TextureMap& texture_map) {
+
+//    link canvas triangle to texture triangle
+    canvas_triangle.v0().texturePoint.x = canvas_triangle.v0().x;
+    canvas_triangle.v0().texturePoint.y = canvas_triangle.v0().y;
+
+    canvas_triangle.v1().texturePoint.x = canvas_triangle.v1().x;
+    canvas_triangle.v1().texturePoint.y = canvas_triangle.v1().y;
+
+    canvas_triangle.v2().texturePoint.x = canvas_triangle.v2().x;
+    canvas_triangle.v2().texturePoint.y = canvas_triangle.v2().y;
+
+//    parse pixel stream to 2d vector
+    std::vector<std::vector<uint32_t>> texture_grid;
+    std::vector<uint32_t> texture_row;
+
+    std::vector<uint32_t> pixels = texture_map.pixels;
+
+    for (int i = 0; i < pixels.size(); i += 1) {
+        texture_row.push_back(pixels[i]);
+
+        if (texture_row.size() == texture_map.width) {
+            texture_grid.push_back(texture_row);
+            texture_row.clear();
+        }
+    }
+
+//    sort canvas and find split point
+    if (canvas_triangle.v1().y < canvas_triangle.v0().y) std::swap(canvas_triangle.v1(), canvas_triangle.v0());
+    if (canvas_triangle.v2().y < canvas_triangle.v0().y) std::swap(canvas_triangle.v2(), canvas_triangle.v0());
+    if (canvas_triangle.v2().y < canvas_triangle.v1().y) std::swap(canvas_triangle.v2(), canvas_triangle.v1());
+
+    float ratio_canvas = (canvas_triangle.v2().x - canvas_triangle.v0().x) / (canvas_triangle.v2().y - canvas_triangle.v0().y);
+    float split_canvas_x = canvas_triangle.v0().x + ratio_canvas * (canvas_triangle.v1().y - canvas_triangle.v0().y);
+
+    CanvasPoint split_point_canvas = CanvasPoint(split_canvas_x, canvas_triangle.v1().y);
+
+//    find proportion of split point along the entire side
+    float split_proportion = getProportionAlongLineGivenPoint(canvas_triangle.v0(), canvas_triangle.v2(), split_point_canvas);
+    CanvasPoint texture_split = getCanvasPixelGivenProportion(canvas_triangle.v0(), canvas_triangle.v2(), split_proportion);
+
+    split_point_canvas.texturePoint.x = texture_split.x;
+    split_point_canvas.texturePoint.y = texture_split.y;
+
+
+    for (int y = canvas_triangle.v0().y; y < canvas_triangle.v2().y; y++) {
+        if (y <= canvas_triangle.v1().y) {
+            float ratio_a = (canvas_triangle.v1().x - canvas_triangle.v0().x) / (canvas_triangle.v1().y - canvas_triangle.v0().y);
+            float x_a = canvas_triangle.v0().x + (y - canvas_triangle.v0().y) * ratio_a;
+            CanvasPoint current_point_a = CanvasPoint(x_a, y);
+            float proportion_along_a = getProportionAlongLineGivenPoint(canvas_triangle.v0(), canvas_triangle.v1(), current_point_a);
+            CanvasPoint point_on_texture_a = getTexturePixelGivenProportion(canvas_triangle.v0(), canvas_triangle.v1(), proportion_along_a);
+
+            float ratio_b = (split_canvas_x - canvas_triangle.v0().x) / (canvas_triangle.v1().y - canvas_triangle.v0().y);
+            float x_b = canvas_triangle.v0().x + (y - canvas_triangle.v0().y) * ratio_b;
+            CanvasPoint current_point_b = CanvasPoint(x_b, y);
+            float proportion_along_b = getProportionAlongLineGivenPoint(canvas_triangle.v0(), split_point_canvas, current_point_b);
+            CanvasPoint point_on_texture_b = getTexturePixelGivenProportion(canvas_triangle.v0(), split_point_canvas, proportion_along_b);
+
+//          draw horizontal scanline
+            for (int x = x_a; x < x_b; x++) {
+                float proportion_along_scan_line = (x - x_a) / (x_b - x_a); // Correct scanline proportion calculation
+                CanvasPoint texture_colour_location = getTexturePixelGivenProportion(point_on_texture_a, point_on_texture_b, proportion_along_scan_line);
+                uint32_t texture_colour = texture_grid[texture_colour_location.texturePoint.y][texture_colour_location.texturePoint.x];
+
+                window.setPixelColour(round(x), round(y), texture_colour);
+            }
+
+        } else {
+            float ratio_a = (canvas_triangle.v2().x - canvas_triangle.v1().x) / (canvas_triangle.v2().y - canvas_triangle.v1().y);
+            float x_a = canvas_triangle.v1().x + (y - canvas_triangle.v1().y) * ratio_a;
+            CanvasPoint current_point_a = CanvasPoint(x_a, y);
+            float proportion_along_a = getProportionAlongLineGivenPoint(canvas_triangle.v1(), canvas_triangle.v2(), current_point_a);
+            CanvasPoint point_on_texture_a = getTexturePixelGivenProportion(canvas_triangle.v1(), canvas_triangle.v2(), proportion_along_a);
+
+            float ratio_b = (canvas_triangle.v2().x - split_canvas_x) / (canvas_triangle.v2().y - canvas_triangle.v1().y);
+            float x_b = split_canvas_x + (y - canvas_triangle.v1().y) * ratio_b;
+            CanvasPoint current_point_b = CanvasPoint(x_b, y);
+            float proportion_along_b = getProportionAlongLineGivenPoint(split_point_canvas, canvas_triangle.v2(), current_point_b);
+            CanvasPoint point_on_texture_b = getTexturePixelGivenProportion(split_point_canvas, canvas_triangle.v2(), proportion_along_b);
+
+//          draw horizontal scanline
+
+            for (int x = x_a; x < x_b; x++) {
+                float proportion_along_scan_line = (x - x_a) / (x_b - x_a); // Correct scanline proportion calculation
+                CanvasPoint texture_colour_location = getTexturePixelGivenProportion(point_on_texture_a, point_on_texture_b, proportion_along_scan_line);
+                uint32_t texture_colour = texture_grid[texture_colour_location.texturePoint.y][texture_colour_location.texturePoint.x];
+
+                window.setPixelColour(round(x), round(y), texture_colour);
+            }
+        }
+    }
+
+
+
+
+}
+
+
 void handleEvent(SDL_Event event, DrawingWindow &window) {
     if (event.type == SDL_KEYDOWN) {
         if (event.key.keysym.sym == SDLK_LEFT) std::cout << "LEFT" << std::endl;
@@ -194,7 +330,6 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 //
 //    SDL_Event event;
 //    while (true) {
-//        // We MUST poll for events - otherwise the window will freeze !
 //        if (window.pollForInputEvents(event)) handleEvent(event, window);
 //        drawLine(
 //                window,
@@ -223,8 +358,6 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 //                CanvasPoint(2 * (WIDTH / 3), HEIGHT / 2),
 //                Colour(255, 255, 255)
 //        );
-//
-//        // Need to render the frame at the end, or nothing actually gets shown on the screen !
 //        window.renderFrame();
 //    }
 //}
@@ -238,7 +371,6 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 //
 //    SDL_Event event;
 //    while (true) {
-//        // We MUST poll for events - otherwise the window will freeze !
 //        if (window.pollForInputEvents(event)) handleEvent(event, window);
 //
 //        if (event.type == SDL_KEYDOWN) {
@@ -255,11 +387,6 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 //
 //            }
 //        }
-//
-//
-//
-//
-//        // Need to render the frame at the end, or nothing actually gets shown on the screen !
 //        window.renderFrame();
 //    }
 //}
@@ -267,36 +394,59 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 
 
 // draw filled triangle
+//int main(int argc, char *argv[]) {
+//
+//    DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
+//
+//    SDL_Event event;
+//    while (true) {
+//        if (window.pollForInputEvents(event)) handleEvent(event, window);
+//
+//        if (event.type == SDL_KEYDOWN) {
+//            if (event.key.keysym.sym == SDLK_f) {
+//                Colour random_colour = Colour(rand() % 256, rand() % 256, rand() % 256);
+//
+//                CanvasPoint random_v0 = CanvasPoint(rand() % WIDTH, rand() % HEIGHT, HEIGHT);
+//                CanvasPoint random_v1 = CanvasPoint(rand() % WIDTH, rand() % HEIGHT, HEIGHT);
+//                CanvasPoint random_v2 = CanvasPoint(rand() % WIDTH, rand() % HEIGHT, HEIGHT);
+//
+//                CanvasTriangle random_triangle = CanvasTriangle(random_v0, random_v1, random_v2);
+//
+//                drawFilledTriangle(window, random_triangle, random_colour);
+//                drawStrokedTriangle(window, random_triangle, Colour(255, 255, 255));
+//            }
+//        }
+//        window.renderFrame();
+//    }
+//}
+
+// draw textured triangle
 int main(int argc, char *argv[]) {
 
     DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 
     SDL_Event event;
     while (true) {
-        // We MUST poll for events - otherwise the window will freeze !
         if (window.pollForInputEvents(event)) handleEvent(event, window);
 
-        if (event.type == SDL_KEYDOWN) {
-            if (event.key.keysym.sym == SDLK_f) {
-                Colour random_colour = Colour(rand() % 256, rand() % 256, rand() % 256);
-
-                CanvasPoint random_v0 = CanvasPoint(rand() % WIDTH, rand() % HEIGHT, HEIGHT);
-                CanvasPoint random_v1 = CanvasPoint(rand() % WIDTH, rand() % HEIGHT, HEIGHT);
-                CanvasPoint random_v2 = CanvasPoint(rand() % WIDTH, rand() % HEIGHT, HEIGHT);
-
-                CanvasTriangle random_triangle = CanvasTriangle(random_v0, random_v1, random_v2);
-
-                drawFilledTriangle(window, random_triangle, random_colour);
-                drawStrokedTriangle(window, random_triangle, Colour(255, 255, 255));
 
 
-            }
-        }
+        CanvasPoint canvas_v0 = CanvasPoint(160, 10);
+        CanvasPoint canvas_v1 = CanvasPoint(300, 230);
+        CanvasPoint canvas_v2 = CanvasPoint(10, 150);
+        CanvasTriangle canvas_triangle = CanvasTriangle(canvas_v0, canvas_v1, canvas_v2);
+
+        CanvasPoint texture_v0 = CanvasPoint(195, 5);
+        CanvasPoint texture_v1 = CanvasPoint(395, 380);
+        CanvasPoint texture_v2 = CanvasPoint(65, 330);
+        CanvasTriangle texture_triangle = CanvasTriangle(texture_v0, texture_v1, texture_v2);
+
+        TextureMap texture_map = TextureMap("texture.ppm");
+
+        drawTexturedTriangle(window, canvas_triangle, texture_triangle, texture_map);
+        drawStrokedTriangle(window, canvas_triangle, Colour(255, 255, 255));
 
 
-
-
-        // Need to render the frame at the end, or nothing actually gets shown on the screen !
         window.renderFrame();
     }
 }
